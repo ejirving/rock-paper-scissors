c0b002c2dee979b02918dec98eb80a76c8a885a9
Eric Irving <ejirving@gmail.com>
2020-04-24 16:11:40 -0400

    CSS added, script functional.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const moves = {
-    //rock paper scissors rules
+//rock paper scissors rules
+const rules = {
     Rock: {weakTo: 'Paper', strongTo: 'Scissors'},
     Paper: {weakTo: 'Scissors', strongTo: 'Rock'},
     Scissors: {weakTo: 'Rock', strongTo: 'Paper'}
 }
 //tracking wins and losses
 let playerWins = 0;
-let playerLosses = 0;
+let computerWins = 0;
 let ties = 0;
 //HTML elements
-const userScore_span = document.getElementById("user-score");
+const playerScore_span = document.getElementById("player-score");
 const tieScore_span = document.getElementById("tie-score");
 const compScore_span = document.getElementById("comp-score");
-const scoreBoard_div = document.querySelector(".scoreboard");
 const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+const resetButton = document.getElementById("reset-button");
 
 //chooses computer's move
 function computerPlay() {
@@ -35,12 +35,12 @@ function playRound(playerSelection, computerSelection) {
     //check for tie first
     if (playerSelection === computerSelection){
         return "T";
-    } else if (moves[playerSelection].strongTo === computerSelection) {
+    } else if (rules[playerSelection].strongTo === computerSelection) {
         return "W";
-    } else if (moves[playerSelection].weakTo === computerSelection) {
+    } else if (rules[playerSelection].weakTo === computerSelection) {
         return "L";
     } else {
-        return "Error! playRound function";
+        console.log("Error! playRound function");
     }
 }
 
@@ -51,33 +51,38 @@ function game(playerSelection){
 
     if (result === "T") {
         ties++;
-        userScore_span.innerHTML = userScore;
         tieScore_span.innerHTML = ties;
-        compScore_span.innerHTML = computerScore;
+        result_div.innerHTML = `${playerSelection} equals ${computerSelection} It's A Draw!`;
     } else if (result === "W") {
-        userScore++;
-        userScore_span.innerHTML = userScore;
-        compScore_span.innerHTML = computerScore;
-        result_div.innerHTML = ``;
+        playerWins++;
+        playerScore_span.innerHTML = playerWins;
+        result_div.innerHTML = `${playerSelection} beats ${computerSelection} You Win! 🏆`;
     } else if (result === "L") {
-        computerScore++;
-        userScore_span.innerHTML = userScore;
-        compScore_span.innerHTML = computerScore;
-        result_div.innerHTML = `${playerSelection} loses to  ${computerSelection}  You Lost! 😔`;
+        computerWins++;
+        compScore_span.innerHTML = computerWins;
+        result_div.innerHTML = `${computerSelection} beats ${playerSelection} You Lost! 😞`;
+    } else {
+        console.log("Error! game function");
     }
 }
 
 function main(){
 
-    rock_div.addEventListener("click", function() {
-        game("Rock");
-    });
+    rock_div.addEventListener("click", function(){game("Rock")});
 
-    paper_div.addEventListener("click", function() {
-        game("Paper");
-    });
+    paper_div.addEventListener("click", function(){game("Paper")});
 
-    scissors_div.addEventListener("click", function() {
-        game("Scissors");
-    });
+    scissors_div.addEventListener("click", function(){game("Scissors")});
+
+    resetButton.addEventListener("click", function(){reset()});
+
 }
+
+function reset(){
+    tieScore_span.innerHTML = ties = 0;
+    playerScore_span.innerHTML = playerWins = 0;
+    compScore_span.innerHTML = computerWins = 0;
+    result_div.innerHTML = ``;
+}
+
+main();
